b00951b55fc4c764947e6b8a88e6421646dd820d
jhclass <jhcode@naver.com>
2022-03-05 01:34:50 +0900

    make it better

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+console.log(path.join(__dirname,'front','build'));
+const reactBuild = path.join(__dirname,'front','build');
+app.use(express.static(reactBuild));
+
+
+app.get('/',async(req,res)=>{
+    res.send({message:"hello"});
+});
+app.get('*',async()=>{
+    res.sendFile(path.join(reactBuild,'index.html'));
+});
+
+app.listen(PORT,()=>console.log('server is running on'+PORT));
